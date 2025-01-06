90a6efea02b15c1b7cbc4a2a92f781a871f876fb
Stephen Nyankson <nyankson25@gmail.com>
2025-01-06 16:20:22 +0000

    Add User app

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
+    'user',
+    
+
 ]
 
 MIDDLEWARE = [
@@ -127,3 +132,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
+
